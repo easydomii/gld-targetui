049df65bf7b742720bd264e34ee2413a8b5df546
GladesH <mathysdefos@gmail.com>
2024-12-11 07:12:44 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+-- Configuration des gains d'XP
+Config.XP = {
+    human = 45,    -- XP pour les humains
+    animal = 10,   -- XP pour les animaux
+    enabled = true -- Active/Désactive le système d'XP
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Glades'
+description 'HUD vie target et personnage'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    "client/*.lua",
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
