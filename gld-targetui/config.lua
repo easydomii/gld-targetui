4cdf1d917aecc3652d8a008c8f6899390c3e54d4
GladesH <mathysdefos@gmail.com>
2024-12-11 07:12:26 +0100

    Delete gld-targetui directory

--- gld-targetui/config.lua
+++
@@ -1,8 +0,0 @@
-Config = {}
-
--- Configuration des gains d'XP
-Config.XP = {
-    human = 45,    -- XP pour les humains
-    animal = 10,   -- XP pour les animaux
-    enabled = true -- Active/Désactive le système d'XP
-}
